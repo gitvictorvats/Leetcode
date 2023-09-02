c29f920f43dbb6d8945baa63bb6f7a7506650e4a
gitvictorvats <61757731+gitvictorvats@users.noreply.github.com>
2023-09-02 00:50:06 -0400

    Time: 1012 ms (5.01%), Space: 16.4 MB (22.6%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.py
+++ 1013-fibonacci-number/1013-fibonacci-number.py
@@ -1,12 +1,36 @@
 class Solution:
+
+    #Recursion without memoization
+    # def fib(self, n: int) -> int:
+
+    #     if n==0 :
+    #         return 0
+        
+    #     if n==1 or n==2:
+    #         return 1
+
+    #     else:
+    #         return self.fib(n-1)+self.fib(n-2)
+
+     #Recursion with memoization
+
     def fib(self, n: int) -> int:
 
         if n==0 :
             return 0
         
-        if n==1 or n==2:
+        if n==1:
             return 1
 
-        else:
-            return self.fib(n-1)+self.fib(n-2)
+        arr=[-1]*(n+1)
+        arr[0]=0
+        arr[1]=1
+
+
+        if arr[n]!=-1:
+            return arr[n]
+
+        arr[n]= self.fib(n-1)+self.fib(n-2)
+
+        return arr[n]
         
