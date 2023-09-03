004fa8f50af9b284ab9cf9492acd245629e20d13
gitvictorvats <61757731+gitvictorvats@users.noreply.github.com>
2023-09-03 15:35:42 -0400

    Time: 1617 ms (12.9%), Space: 27.3 MB (23.32%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,20 +1,23 @@
-from functools import lru_cache
-
-
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
 
-        @lru_cache(None)
-        def dfs(rem):
-            if rem < 0:
-                return -1
-            if rem == 0:
+        def minCoins(c, n, V, memo):
+            if V == 0:
                 return 0
-            min_cost = float('inf')
-            for coin in coins:
-                res = dfs(rem - coin)
-                if res != -1:
-                    min_cost = min(min_cost, res + 1)
-            return min_cost if min_cost != float('inf') else -1
+            if n == 0:
+                return float('inf')
+            if memo[n][V] != -1:
+                return memo[n][V]
 
-        return dfs(amount)
+            if c[n - 1] <= V:
+                memo[n][V] = min(1 + minCoins(c, n, V - c[n - 1], memo), minCoins(c, n - 1, V, memo))
+            else:
+                memo[n][V] = minCoins(c, n - 1, V, memo)
+            
+            return memo[n][V]
+
+        size = len(coins)
+        memo = [[-1 for _ in range(amount + 1)] for _ in range(size + 1)]
+        
+        result = minCoins(coins, size, amount, memo)
+        return result if result != float('inf') else -1
