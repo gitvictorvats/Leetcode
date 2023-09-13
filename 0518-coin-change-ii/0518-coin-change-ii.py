5015169f3066f5468094f442d8091e5b5cd84847
gitvictorvats <61757731+gitvictorvats@users.noreply.github.com>
2023-09-13 01:37:10 -0400

    Time: 354 ms (41.72%), Space: 32.1 MB (39.46%) - LeetHub

---
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -0,0 +1,27 @@
+from typing import List
+
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        
+        N = len(coins)
+        
+        if N == 0:
+            return 0 if amount > 0 else 1
+        
+        dp = [[0 for _ in range(amount + 1)] for _ in range(N + 1)]
+
+        for i in range(N + 1):
+            dp[i][0] = 1
+
+        def changeHelper(amount, coins, N):
+            j=0
+            for i in range(1, N + 1):
+                for j in range(1, amount + 1):
+                    if coins[i - 1] <= j:
+                        dp[i][j] = dp[i][j - coins[i - 1]] + dp[i - 1][j]
+                    else:
+                        dp[i][j] = dp[i - 1][j]
+
+            return dp[N][amount]
+        
+        return changeHelper(amount, coins, N)
