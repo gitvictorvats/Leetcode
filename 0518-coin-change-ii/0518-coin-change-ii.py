64694744b5178523d9fcadfdea60f12208a7bae5
gitvictorvats <61757731+gitvictorvats@users.noreply.github.com>
2023-09-14 13:08:45 -0400

    Time: 154 ms (65.5%), Space: 30 MB (44.89%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.py
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -1,27 +1,43 @@
-from typing import List
 
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
         
-        N = len(coins)
-        
-        # if N == 0:
-        #     return 0 if amount > 0 else 1
-        
-        dp = [[0 for _ in range(amount + 1)] for _ in range(N + 1)]
+        def chnageHelper(amount,coins,N,memo):
 
-        for i in range(N + 1):
-            dp[i][0] = 1
-
-        def changeHelper(amount, coins, N):
             
-            for i in range(1, N + 1):
-                for j in range(1, amount + 1):
-                    if coins[i - 1] <= j:
-                        dp[i][j] = dp[i][j - coins[i - 1]] + dp[i - 1][j]
-                    else:
-                        dp[i][j] = dp[i - 1][j]
 
-            return dp[N][amount]
+            # base condition
+
+            if amount==0 :
+                return 1
+            
+            if N==0 :
+                return 0
+
+            if memo[N][amount]!=-1:
+
+                return memo[N][amount]
+            
+            # include case
+
+            if (coins[N-1]<=amount):
+
+                include= chnageHelper(amount-coins[N-1],coins,N,memo)
+                exclude= chnageHelper(amount,coins,N-1,memo)
+
+                memo[N][amount]=include + exclude
+                return memo[N][amount]
+
+
+            else:
+                exclude= chnageHelper(amount,coins,N-1,memo)
+                memo[N][amount]=exclude
+                return memo[N][amount]
         
-        return changeHelper(amount, coins, N)
+        
+        N=len(coins)
+        memo=[[-1 for _ in range(amount+1)] for _ in range(N+1)]
+        return chnageHelper(amount,coins,N,memo)
+
+
+            
