f11c2cfb9da4834e94024b072f9cacb3c8d2b039
gitvictorvats <61757731+gitvictorvats@users.noreply.github.com>
2023-09-23 00:55:26 -0400

    Time: 880 ms (73.36%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
-select t1.id
+select t1.id#,t1.recordDate,t1.temperature,t2.id,t2.recordDate,t2.temperature
 from Weather t1
 join Weather t2
 on DATEDIFF(t1.recordDate, t2.recordDate) = 1
