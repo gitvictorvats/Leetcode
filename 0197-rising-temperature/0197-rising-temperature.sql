bb3b1547c75f25843a6536abff0ad9efafa1a00e
gitvictorvats <61757731+gitvictorvats@users.noreply.github.com>
2023-10-05 15:50:31 -0400

    Time: 825 ms (78.29%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,6 +1,9 @@
 # Write your MySQL query statement below
-select t1.id#,t1.recordDate,t1.temperature,t2.id,t2.recordDate,t2.temperature
-from Weather t1
-join Weather t2
-on DATEDIFF(t1.recordDate, t2.recordDate) = 1
-where t1.temperature>t2.temperature
+
+select T2.id
+from Weather as T1
+JOIN Weather T2
+ON DATEDIFF(T2.recordDate,T1.recordDate)=1
+where T2.temperature > T1.temperature
+
+
