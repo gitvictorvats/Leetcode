c5c0b373ba0838a850be5ffbc28f4d518e8acaea
gitvictorvats <61757731+gitvictorvats@users.noreply.github.com>
2023-10-05 18:58:29 -0400

    Time: 503 ms (89.69%), Space: 0B (100%) - LeetHub

---
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -0,0 +1,31 @@
+# Write your MySQL query statement below
+
+# with subQuery as (
+
+# select E2.name as Manager_name,E1.managerId as Manager ,E1.id as reportee
+# from Employee E1
+# LEFT JOIN Employee E2
+# ON E1.managerId=E2.id
+
+# )
+
+
+
+SELECT E2.name
+FROM Employee E1
+JOIN Employee E2 ON E1.managerId = E2.id
+GROUP BY E2.id, E2.name
+HAVING COUNT(E1.id) >= 5;
+
+
+
+
+
+
+
+
+
+
+
+
+
