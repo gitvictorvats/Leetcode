4b4f5fe6a88f08871476ee08833ae124cb195e67
gitvictorvats <61757731+gitvictorvats@users.noreply.github.com>
2023-10-06 02:25:57 -0400

    Time: 61 ms (10.19%), Space: 18.4 MB (99.01%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.py
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.py
@@ -1,35 +1,33 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution:
+    
+    def __init__(self):
+        self.memo = {}
 
-    def calulateDepth(self,root):
+    def calulateDepth(self, root):
+        if root == None:
+            return 0
 
-            if root ==None:
-                return 0
+        if root in self.memo:
+            return self.memo[root]
 
-            return max(1+self.calulateDepth(root.right),1+self.calulateDepth(root.left))  
+        left_depth = self.calulateDepth(root.left)
+        right_depth = self.calulateDepth(root.right)
+        
+        # Store the computed depth in the memo dictionary
+        self.memo[root] = 1 + max(left_depth, right_depth)
+        
+        return self.memo[root]
 
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
-
-        if root==None:
+        if root == None:
             return 0
 
         # Diameter passing through root
         diameter_through_root = self.calulateDepth(root.right) + self.calulateDepth(root.left)
-        
-        # Diameter entirely in left subtree
+
+        # Diameter entirely in left subtree and right subtree
         diameter_left = self.diameterOfBinaryTree(root.left)
-        
-        # Diameter entirely in right subtree
         diameter_right = self.diameterOfBinaryTree(root.right)
-        
+
         # Return the maximum of the three possible diameters
         return max(diameter_through_root, diameter_left, diameter_right)
-
-
-      
-        
