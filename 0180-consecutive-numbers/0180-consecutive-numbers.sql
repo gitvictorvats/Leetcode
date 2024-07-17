33083cc92664cbb0b60bb25db3fbc9d4aeb6ad6e
gitvictorvats <61757731+gitvictorvats@users.noreply.github.com>
2024-07-16 22:19:37 -0400

    Time: 610 ms (42.63%), Space: 0B (100%) - LeetHub

--- 0180-consecutive-numbers/0180-consecutive-numbers.sql
+++ 0180-consecutive-numbers/0180-consecutive-numbers.sql
@@ -1,12 +1,12 @@
-SELECT DISTINCT
-    l1.Num AS ConsecutiveNums
-FROM
-    Logs l1,
-    Logs l2,
-    Logs l3
-WHERE
-    l1.Id = l2.Id - 1
-    AND l2.Id = l3.Id - 1
-    AND l1.Num = l2.Num
-    AND l2.Num = l3.Num
-;
+-- select t1.id,t1.num,t2.id,t2.num,t3.id,t3.num
+-- from Logs t1
+-- join logs t2 on t1.id = t2.id-1
+-- join logs t3 on t2.id-1 = t3.id-2
+
+-- where t1.num=t2.num and t2.num=t3.num
+select distinct t1.num as ConsecutiveNums
+from Logs t1
+join logs t2 on t1.id = t2.id-1
+join logs t3 on t2.id-1 = t3.id-2
+
+where t1.num=t2.num and t2.num=t3.num
