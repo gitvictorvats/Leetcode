6f9f64ca154dcdc8cf4de070e98b44c114f63b61
gitvictorvats <61757731+gitvictorvats@users.noreply.github.com>
2025-01-10 02:04:52 -0500

    Time: 455 ms (43.81%), Space: 0B (100%) - LeetHub

--- 0178-rank-scores/0178-rank-scores.sql
+++ 0178-rank-scores/0178-rank-scores.sql
@@ -1,6 +1,11 @@
-/* Write your T-SQL query statement below */
+
+with cte as (
+select id,score, dense_rank() over (order by score desc) as ranked_score
+from scores
+)
+
+select score ,ranked_score as 'rank'
+from cte
+order by score desc
 
 
-select score, DENSE_RANK () over(order by score desc) as rank
-from Scores
-order by rank asc
