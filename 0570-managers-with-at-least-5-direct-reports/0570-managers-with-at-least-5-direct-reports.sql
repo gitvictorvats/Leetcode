8ca98d77cf3a77aa181a0ee127e3b2940547ebcc
gitvictorvats <61757731+gitvictorvats@users.noreply.github.com>
2025-01-11 01:11:26 -0500

    Time: 372 ms (70.56%), Space: 0B (100%) - LeetHub

--- 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -1,31 +1,14 @@
 # Write your MySQL query statement below
 
-# with subQuery as (
+with manager_table as (
 
-# select E2.name as Manager_name,E1.managerId as Manager ,E1.id as reportee
-# from Employee E1
-# LEFT JOIN Employee E2
-# ON E1.managerId=E2.id
+select managerid,count(case when name then 1 else 0 end) as count_reportee
+from employee
+group by managerid
+)
 
-# )
-
-
-
-SELECT E2.name
-FROM Employee E1
- JOIN Employee E2 ON E1.managerId = E2.id
-GROUP BY E2.id
-HAVING COUNT(E1.id) >= 5;
-
-
-
-
-
-
-
-
-
-
-
-
-
+select t2.name as name
+from manager_table t1
+join Employee t2
+on t1.managerid=t2.id
+where count_reportee>=5 and t1.managerid is not null
